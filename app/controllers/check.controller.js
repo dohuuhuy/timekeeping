0c02386f175336eada9eebcb498e0784a1bc7289
dohuuhuy <dohuuhuy2@gmail.com>
2020-10-22 12:52:39 +0700

    dvds

--- app/controllers/check.controller.js
+++ app/controllers/check.controller.js
@@ -249,7 +249,7 @@ exports.findAll = (req, res) => {
     })
     .catch((err) => {
       res.status(500).send({
-        message: err.message || "Some error occurred while retrieving checkss.",
+        message: err.message || "Some error occurred while retrieving .",
       });
     });
 };
