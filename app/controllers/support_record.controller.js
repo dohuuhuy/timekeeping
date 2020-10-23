ce59f32044eeabf06403bec8b0e1d991bc14b10e
huy <54833364+dohuuhuy@users.noreply.github.com>
2020-10-23 08:13:30 +0700

    ok

--- app/controllers/support_record.controller.js
+++ app/controllers/support_record.controller.js
@@ -1,78 +1,105 @@
-const support_record = require("../models/support_record.model");
+const Support_record = require("../models/support_record.model");
 const moment = require("moment");
-exports.create = (req, res) => {
-  // Validate request
-  //  if(!req.body.content) {
-  //     return res.status(400).send({
-  //         message: "Note content can not be empty"
-  //     });
-  // }
+const axios = require("axios");
 
-  var date = new Date();
-  var curDate = moment(date).format("YYYY-MM-DD HH:mm:ss");
-
-  console.log("curDate :>> ", curDate);
-
-  const note = new support_record({
-    supporterId: req.body.supporterId,
-    userCode: req.body.userCode,
-    time: date,
-    bookingCode: req.body.bookingCode,
-    verifyStatus: req.body.verifyStatus,
-    verifyDescription: req.body.verifyDescription,
-  });
-
-  note
-    .save()
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || "Some error occurred while creating the Note.",
-      });
-    });
-};
-
-exports.findAll = async (req, res) => {
-  const axios = require("axios");
-  const moment = require("moment");
-
-  var dta = await axios.get(
-    "https://medpro-api-v2-testing.medpro.com.vn/booking-gateway/get-booking-by-transaction-code?transactionId=VPDev201021CCLNJOQPYFEL"
-  );
-
-  let data = dta.data;
-  var x1 = data.bookingInfo.createdAt;
-  var y1 = new Date();
-  var x = moment(x1);
-  var y = moment(y1);
-
-  console.log("data.bookingInfo.createdAt :>> ", x);
-  console.log("curent time                :>> ", y);
-
-  var diffInMinutes = y.diff(x, "minutes");
-  console.log("thời gian tạo :>> ", diffInMinutes, "phút trước");
-
-  if (diffInMinutes <= 10) {
-    console.log("--> phiếu tạo trong 10 phút");
-  } else {
-    console.log("--> phiếu đã tạo", diffInMinutes, "phút trước");
+checkUserIsNew = async (req, res) => {
+  try {
+    var dta = await axios.get(
+      "https://medpro-api-v2-testing.medpro.com.vn/booking-gateway/get-booking-by-transaction-code?transactionId=VPDev201021CCLNJOQPYFEL"
+    );
+    let data = dta.data;
+    var x1 = data.bookingInfo.createdAt;
+  
+    var y1 = new Date();
+    var x = moment(x1);
+    var y = moment(x1);
+    var diffInMinutes = y.diff(x, "minutes");
+    var listBook = 1;
+  
+    console.log("data.bookingInfo.createdAt :>> ", x);
+    console.log("curent time                :>> ", y);
+    console.log("thời gian tạo :>> ", diffInMinutes, "phút trước");
+    console.log("Mã bệnh nhân :>> ", data.patientInfo.code);
+    console.log("Mã phiếu khám :>> ", data.bookingInfo.bookingCode);
+  
+    if (diffInMinutes > 10) {
+      console.log("--> phiếu đã tạo", diffInMinutes, "phút trước");
+      res.send({ success: false, message: "Phiếu đã tạo " + diffInMinutes + " phút trước" });
+      //  return {success: false, message:"Phiếu đã tạo" + diffInMinutes + "phút trước"};
+    }
+    if (listBook != 1) {
+      res.send({ success: false, message: "List > 1 " });
+      //  return {success: false, message:"List > 1 "}
+    }
+    else {
+  
+      return { success: true, message: "Thành công", }
+    }
+  
+    handleClearData();
+  } catch (error) {
+    return { success: false};
   }
 
-  console.log("Mã bệnh nhân :>> ", data.patientInfo.code);
-  console.log("Mã phiếu khám :>> ", data.bookingInfo.bookingCode);
+}
 
-  var x = await support_record.find({});
-  console.log("x :>> ", x);
+exports.create = async (req, res) => {
+
+
+  var date = new Date();
+
+  var _checkUserIsNew = await checkUserIsNew(req, res);
+  if (_checkUserIsNew.success === false) {
+
+    res.send({ success: false, message: "không co dữ liệu để kiểm tra" });
+  }
+
+else
+{
+  const support_record = new Support_record({
+      supporterId: req.body.supporterId,
+      userCode: req.body.userCode,
+      time: date,
+      bookingCode: req.body.bookingCode,
+      verifyStatus: 0,
+      verifyDescription: req.body.verifyDescription,
+    });
+
+    support_record
+      .save()
+      .then((data) => {
+        res.send({ success: true, message: "post thành công !", data: data });
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || "Some error occurred while creating the Note.",
+        });
+      });
+}
     
+
+  
+
+
+
+};
+
+exports.findAll = (req, res) => {
+
+
+
+  Support_record.find({}).then((data) => {
+    res.send(data);
+  })
+
+
 
 };
 
 exports.findOne = (req, res) => {
   var id = req.params.sp_recordId;
 
-  support_record
+  Support_record
     .find({ supporter: id })
     .then((note) => {
       if (!note) {
@@ -101,7 +128,7 @@ exports.update = (req, res) => {
     });
   }
 
-  support_record
+  Support_record
     .findByIdAndUpdate(
       req.params.noteId,
       {
@@ -131,7 +158,7 @@ exports.update = (req, res) => {
 };
 
 exports.delete = (req, res) => {
-  support_record
+  Support_record
     .findByIdAndRemove(req.params.noteId)
     .then((note) => {
       if (!note) {
--- app/models/workship.model.js
+++ app/models/workship.model.js
@@ -1,25 +1,26 @@
 const mongoose = require('mongoose');
 
-const NoteSchema = mongoose.Schema({
-   
-    partnerId: String,
-    locationId: String,
-    workshipId: String,
-    code: String,
-    shiftId: Number,
-    shiftName: String,
-    fromTime: String,
-    toTime: String,
-    customSlot: String,
-    days: String,
-    type: String,
-    status: Number,
-    createTime: {
-        type: Date,
-        default: Date.now
-    },
-    
-
+const workshipSchema = mongoose.Schema({
+  partnerId: String,
+  locationId: String,
+  workshipId: String,
+  code: String,
+  shiftId: Number,
+  shiftName: String,
+  fromTime: String,
+  toTime: String,
+  customSlot: String,
+  days: String,
+  type: String,
+  status: Number,
+  createTime: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model("work_schedules", NoteSchema, "work_schedules");
+module.exports = mongoose.model(
+  "work_schedules",
+  workshipSchema,
+  "work_schedules"
+);
