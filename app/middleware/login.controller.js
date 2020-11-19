260d78404e0901df39c6c53dea34230a3490d7ae
dohuuhuy <dohuuhuy2@gmail.com>
2020-11-19 11:24:37 +0700

    ok

--- app/middleware/login.controller.js
+++ app/middleware/login.controller.js
@@ -17,29 +17,30 @@ exports.post_Login = async (req, res) => {
 
   const url =
     "https://medpro-api-v2-testing.medpro.com.vn/v1/userAccount/login";
+
   var x = await axios.post(url, data, options);
   // console.log("x :>> ", x.data);
   res.send(x.data);
 };
 
 exports.check_token_login = async (req, res, next) => {
-  console.log('req.headers',req.headers)
+  console.log("req.headers", req.headers);
   const options = {
     headers: {
       accept: "/",
-      partnerid: "trungvuong",
+     // partnerid: "trungvuong",
       Authorization: `${req.headers.authorization}`,
     },
   };
   try {
-    const url =
-      "https://medpro-api-v2-testing.medpro.com.vn/v1/userAccount/can-bo-yte/info";
+    const url = "https://medpro-api-v2-testing.medpro.com.vn/user/info";
+
     var x = await axios.get(url, options);
     console.log("x.data", x.data.userId);
     res.locals.userId = x.data.userId;
     return next();
   } catch (error) {
     console.log(error);
-    res.status(401).json({ message: "No author user" });
+    res.status(401).json({statusCode:401, message: "No author user" });
   }
 };
